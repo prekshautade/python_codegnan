bce1afb821a1d89393e45379f208ce7b2d6a6f2c
Preksha <prekshautade94@gmail.com>
2025-10-10 17:33:32 +0530

    Delete f format.py

--- f format.py
+++
@@ -1,8 +0,0 @@
-name = "ravi"
-age = 20
-percentage = 95.6926
-print(f"Name is {name} and age is {age} and i have {percentage}")
-print(f"Name is {name} and age is {age} and i have {percentage:.2f}")
-num = 1
-print(f"{num:0{5}d}")
-print(f"{num:4{2}d}")
